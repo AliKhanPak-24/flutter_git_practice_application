6d6f9f3479f819c5ae38df6d34b6fe572ebac491
AliKhanPak-24 <ali.hassan.pakbrains@gmail.com>
2026-07-29 10:45:33 +0500

    created the settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -70,6 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            ElevatedButton(onPressed: () {}, child: Text("Settings")),
           ],
         ),
       ),
